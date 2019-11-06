6a12d4e8b6e724c45d539dabc6ef557beb2689e1
Daniel Collins <danielmcollins1996@gmail.com>
2019-11-05 21:56:46 -0800

    router nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PostList from '../src/components/PostList';
+import Toolbar from './components/Toolbar/Toolbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Toolbar />
       <PostList />
      
     </div>
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
+const toolbar = props => (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Players</Link>
+          </li>
+          <li>
+            <Link to="/users">Teams</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/Players">
+          {/* <About /> */}
+        </Route>
+        <Route path="/Teams">
+          {/* <Users /> */}
+        </Route>
+        <Route path="/">
+          {/* <Home /> */}
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default toolbar;
