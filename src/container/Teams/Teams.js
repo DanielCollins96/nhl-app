5d8b6ffc4f7111cbbce5d1f28c40bbf8ed8d1fd4
Daniel Collins <Daniel.Collins@Tolko.com>
2019-12-12 16:35:26 -0800

    added data to the state

--- src/container/Teams/Teams.js
+++ src/container/Teams/Teams.js
@@ -1,30 +1,55 @@
-import React, { Component,useState, useEffect } from 'react';
+import React, { Component, useState, useEffect,setState } from 'react';
 
 import TeamLogos from '../../data/cleanLogos.json'
 import classes from './Teams.css';
 
-const Teams = () => {
+class Teams extends Component {
 
-    console.log(TeamLogos.teams)
+    state = {
+        team_list: TeamLogos.teams
+    }
+    
+    sortAscending = () => {
+        console.log(...this.state.team_list)
+        const name_val = this.state.team_list
+        // console.log(this.state.team_list[1].name)
+        name_val.sort(function(a,b){
+            console.log(a.franchiseId)
+            return a.franchiseId - b.franchiseId
+        })
+        // this.setState
+        // this.state.team_list.map(nm => console.log(nm.name))
+        this.setState({
+            team_list: name_val
+        })
+        console.log(name_val)
+        // name.sort()
+        // this.setState({ name })
+    }
+    
+    render() {
+        console.log(this.state)
+        console.log(this.state.team_list)
 
-    console.log(TeamLogos.teams)
         return (
             <div className="teampage">
                 {/* <h1>{TeamLogos.teams[0].name}</h1> */}
+                <button onClick={this.sortAscending}>HI</button>
                 <div className="Teams">
-                    {TeamLogos.teams.map((tm, index)=>{
+                    {this.state.team_list.map((tm, index) => {
                         return <div className="Player">
-                        <h2>{tm.name}</h2>
-                        <img src={tm.logo} height="100px"alt="Player Headshot"/>
-                        <p>{tm.conference}</p>
-                        <p>{tm.position}</p>
+                            <h2>{tm.name}</h2>
+                            <img src={tm.logo} height="100px" alt="Player Headshot" />
+                            <p>{tm.conference}</p>
+                            <p>{tm.position}</p>
                         </div>
                     })}
                 </div>
-                
+
             </div>
         );
-    };
+    }
+};
 
 
 export default Teams;
